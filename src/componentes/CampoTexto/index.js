fd236bcf3f365d8b5fd971082ab9af179f793a6f
Lucas Adriano de Souza <104021672+LucasAdrian0@users.noreply.github.com>
2025-10-04 07:19:41 -0300

    subindo arquivos atualizados

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './CampoTexto.css'
+
+const CampoTexto = (props) => {
+
+  const placeholderModificado = `${props.placeholder}...`
+
+  return (
+    <div className='campo-texto'>
+
+      <label>{props.label}</label>
+      <input type="text" placeholder={placeholderModificado}/>
+        
+    </div>
+  )
+}
+
+export default CampoTexto
